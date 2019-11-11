037a8add75538d98e60718b1a69b3334e4d6c6aa
nagayev <sidorovmarat1995@gmail.com>
2019-11-11 20:52:31 +0300

    debug

--- test2.js
+++ test2.js
@@ -3,6 +3,8 @@ fetch('test.wasm').then(response =>
   response.arrayBuffer()
 ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
   instance = results.instance;
+  console.log(instance);
   exports=instance.exports;
+  console.log(exports);
 }).catch(console.error);
 
