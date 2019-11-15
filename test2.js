a6655c2565dcacc41f3c8f19b9587a8ece7c3aca
nagayev <sidorovmarat1995@gmail.com>
2019-11-15 20:24:58 +0300

    Try main in test2

--- test2.js
+++ test2.js
@@ -3,8 +3,6 @@ fetch('test.wasm').then(response =>
   response.arrayBuffer()
 ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
   instance = results.instance;
-  console.log(instance);
-  exports=instance.exports;
-  console.log(exports);
+  instance.exports.main();
 }).catch(console.error);
 
